f3e23a019fddc320cbde898294b948c7f0903e8d
junbuluv <junbuluv@gmail.com>
2022-04-10 00:15:28 -0400

    Update main.m

--- main.m
+++ main.m
@@ -129,15 +129,16 @@ P_a = sparse(P_a);
 
 tol_mu = 10e-6;
 dif_mu =Inf;
-mu_init = rand(n_z*n_dis,1)./n_dis;
+mu_init = rand(1,n_dis)'./n_dis;
+mu_init = repmat(mu_init,n_z,1);
 
-
+%% Need to fix
 while dif_mu > tol_mu
     mu_ap = P_a * mu_init ;
     mu_ap = reshape(mu_ap,[n_dis, n_z]);
-    mu_apz = mu_ap*p_z';
+    mu_ap_z = mu_ap * p_z';
 
-    mu_update = reshape(mu_apz, [n_z*n_dis, 1]);
+    mu_update = reshape(mu_ap_z, [n_z*n_dis, 1]);
 
     dif_mu=norm(mu_init-mu_update,Inf)
     mu_init =mu_update;
